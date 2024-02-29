9087e372881538a8e75c3afcbc9d45e7c4e24b9c
logbasem <loganmichellemurphy@gmail.com>
2024-02-28 19:21:02 -0600

    Created video player component with test url, need to get file urls working

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
+import ReactPlayer from 'react-player';
 import './App.css';
 
+//filepath for testing (DELETE LATER): ../../../GitHub/Automomous/examples/ARTrackerTest/videos
 function App() {
   const [sliderValue, setSliderValue] = useState(50); // Initial slider value
+  const [videoFilePath] = useState("https://youtu.be/WLw4mNk6fTk?si=9-LhGRexKvEPWMQ1"); // Initial video file path
 
   return (
     <div className="App">
       <header className="App-header">
+      <ReactPlayer url={videoFilePath} width="90%" height="90%" playing={true} volume={0} controls={true} />
         <input
           type="range"
           min="1"
